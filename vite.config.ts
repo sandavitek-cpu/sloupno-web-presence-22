60c19563060055ed918aac724dac811f75f21f1b
sandavitek-cpu <sanda.vitek@gmail.com>
2026-02-07 09:28:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    base: '/sloupno-web-presence-22/',
   server: {
     host: "::",
     port: 8080,
